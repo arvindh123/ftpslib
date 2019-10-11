406a0313a9ca25aae418aea4dd356a335223125a
Arvindh <30824765+arvindh123@users.noreply.github.com>
2019-10-11 15:28:51 +0530

    Update ftpslib.py

--- ftpslib.py
+++ ftpslib.py
@@ -1,22 +1,33 @@
 import ssl, socket , ftplib
 class ftps_implicit(ftplib.FTP_TLS):
-    """FTP_TLS subclass that automatically wraps sockets in SSL to support implicit FTPS."""
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._sock = None
-
     @property
     def sock(self):
-        """Return the socket."""
         return self._sock
-
     @sock.setter
     def sock(self, value):
-        """When modifying the socket, ensure that it is ssl wrapped."""
         if value is not None and not isinstance(value, ssl.SSLSocket):
             value = self.context.wrap_socket(value)
         self._sock = value
+    def CustStorBinary(self, cmd, fp, blocksize=8192, callback=None, rest=None):
+        self.voidcmd('TYPE I')
+        with self.transfercmd(cmd, rest) as conn:
+            while 1:
+                buf = fp.read(blocksize)
+                if not buf:
+                    break
+                conn.sendall(buf)
+                if callback:
+                    callback(buf)
+            # shutdown ssl layer
+            
+            # if _SSLSocket is not None and isinstance(conn, _SSLSocket):
+            #     # conn.unwrap()
+            #     pass
+           
+        return self.voidresp()
 
 # ftp_client = ftps_implicit()
 # ftp_client.connect(host='ftp.example.com ', port=990)
